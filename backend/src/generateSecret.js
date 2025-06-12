0d8f33f3b595151c26af035c193433fd01505b28
satyabrat <satyabrat34@gmail.com>
2025-06-12 20:04:56 +0530

    ready for deployment

--- backend/src/generateSecret.js
+++
@@ -1,5 +0,0 @@
-import crypto from 'crypto';
-
-// Generate a random string of 32 bytes and convert to base64
-const secret = crypto.randomBytes(32).toString('base64');
-console.log('Generated Secret:', secret); 
