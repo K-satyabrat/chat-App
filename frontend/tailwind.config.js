e0ca0f2433d0b74d33b756600d98991f7c579c32
satyabrat <satyabrat34@gmail.com>
2025-06-13 06:06:30 +0530

    ready for deployment

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui'
+import daisyui from "daisyui";
 
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
@@ -14,7 +11,14 @@ export default {
     themes: [
       "light",
       "dark",
-      
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+   
     ],
   },
-} 
+};
